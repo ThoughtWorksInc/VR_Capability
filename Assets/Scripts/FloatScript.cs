3fcd3f63bf61b124985a1d3dbecf23011e6c1f55
Jacob <jacobfrom1993@ymail.com>
2016-07-13 15:00:27 +1000

    Repositioned the pickup and added hover script

---
+++ Assets/Scripts/FloatScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatScript : MonoBehaviour {
+    Vector3 startPos;
+    float delta = 1f;
+    float speed = 2f;
+
+	// Use this for initialization
+	void Start () {
+        startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 v = startPos;
+        v.y += Mathf.Abs(delta * Mathf.Sin(Time.time * speed));
+        transform.position = v;
+    }
+}
