e7504424cd8b096beffb8514a5e11e22673f57a0
Steve M <smorris@thoughtworks.com>
2016-07-18 16:28:06 +1000

    SM: Added placeholder unit tests

---
+++ Assets/Editor/FailingTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AssemblyCSharp
+{
+	public class FailingTest
+	{
+		[Test]
+		public void ShouldFail()
+		{
+			Assert.Fail();
+		}
+	}
+}
+
---
+++ Assets/Editor/PassingTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AssemblyCSharp
+{
+	public class PassingTest
+	{
+		[Test]
+		public void ShouldPass()
+		{
+			Assert.Pass();
+		}
+	}
+}
+
