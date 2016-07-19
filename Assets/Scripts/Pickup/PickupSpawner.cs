9622e91a3d05134cfee342160ed7937f56fe6e9b
Dominique Rolin <drolin@thoughtworks.com>
2016-07-19 16:30:09 +1000

    spawn pickups progressively

--- Assets/Scripts/Pickup/PickupSpawner.cs
+++ Assets/Scripts/Pickup/PickupSpawner.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Threading;
 using Zenject;
 
 public class PickupSpawner : MonoBehaviour
@@ -7,6 +8,10 @@ public class PickupSpawner : MonoBehaviour
 
     private PickupFacade.Factory _factory;
     private Vector3 originPosition = new Vector3(0f,0.5f,0f);
+    private int _maxAmountOfPickups;
+    private float _angle;
+    private int _pickupsOnScene = 0;
+    private int _cooldown;
 
     [Inject]
     public void Construct(PickupFacade.Factory factory)
@@ -14,14 +19,26 @@ public class PickupSpawner : MonoBehaviour
         _factory = factory;
     }
 
-    public void SpawnPickups(int numberOfPickups)
+    public void SpawnPickups(int amount)
     {
-        var angle = 2*Mathf.PI/numberOfPickups;
-        for (var i = numberOfPickups; i > 0; i--)
+        _maxAmountOfPickups = amount;
+        _angle = 2 * Mathf.PI / _maxAmountOfPickups;
+
+    }
+
+    public void FixedUpdate()
+    {
+        if (_cooldown-- > 0) return;
+        if (_pickupsOnScene < _maxAmountOfPickups)
         {
-            var pickup = _factory.Create();
-            var theta = angle * i;
-            pickup.Position = originPosition +  Vector3.right * Mathf.Cos(theta) * 5 + Vector3.forward * Mathf.Sin(theta) * 5;
+            SpawnPickup();
         }
+    }
+    private void SpawnPickup()
+    {
+        _cooldown = 5;
+        var pickup = _factory.Create();
+        var theta = _angle * _pickupsOnScene++;
+        pickup.Position = originPosition + Vector3.right * Mathf.Cos(theta) * 5 + Vector3.forward * Mathf.Sin(theta) * 5;
     }
 }
