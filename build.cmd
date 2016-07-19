595015cdc691db881dae062f44cea6023020dbef
Dominique Rolin <drolin@thoughtworks.com>
2016-07-19 11:08:57 +1000

    exit cmd with proper error code

--- build.cmd
+++ build.cmd
@@ -1,3 +1,6 @@
-
+@echo off
 REM Windows build script calls osx/linux build script via Git bash.
 "%ProgramFiles%\Git\bin\sh.exe" ./build.sh
+if "%errorlevel%" neq "0" (
+    exit /b %errorlevel%
+)
