50c69ca0e37aa061b9d0186763c03da1bbef2076
Dominique Rolin <drolin@thoughtworks.com>
2016-07-19 12:08:28 +1000

    fix error handling in bash script, rename variables for unity path

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
-: "${UNITY_HOME:?Need to set UNITY_HOME. See set-local-build-vars-*.sh}"
+#/bin/sh 
+set -ex
+
+: "${UNITY_PATH:?Need to set UNITY_HOME. See set-local-build-vars-*.sh}"
 : "${PROJECT_PATH:=.}"
-: "${LOG_FILENAME:=unity.build.log}"
 : "${BUILD_NUMBER:=0}"
+: "${LOG_FILENAME:=unity.build.$BUILD_NUMBER.log}"
+
+echo "using UNITY_PATH '$UNITY_PATH'"
+echo "using PROJECT_PATH '$PROJECT_PATH'"
+echo "using LOG_FILENAME '$LOG_FILENAME'"
+echo "using BUILD_NUMBER '$BUILD_NUMBER'"
 
 echo "build.sh : starting..."
-"$UNITY_HOME/Unity" -projectPath "$PROJECT_PATH" -logfile "$PROJECT_PATH/$LOG_FILENAME" -executeMethod AndroidBuilder.Build -buildNumber "$BUILD_NUMBER" -batchmode -nographics -quit
-echo "build.sh : end."
+"$UNITY_PATH/Unity" -projectPath "$PROJECT_PATH" -logfile "$PROJECT_PATH/$LOG_FILENAME" -executeMethod AndroidBuilder.Build -buildNumber "$BUILD_NUMBER" -batchmode -nographics -quit
+echo "build.sh done"
